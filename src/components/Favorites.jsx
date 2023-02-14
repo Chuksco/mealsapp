ef6265c4693c55806a77c98e3250ce0f53c48dde
Onu Chukwukamso <70770519+Chuksco@users.noreply.github.com>
2023-02-14 11:57:16 +0000

    Update

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,4 +1,27 @@
+import { useGlobalContext } from '../context'
+import { MdCancel } from 'react-icons/md'
+
+
+
 const Favorites = () => {
-  return <h1>Favorites</h1>
+  const { favorites, selectMeal, removeFromFavorites } = useGlobalContext()
+
+  return <section className="favorites">
+    <div className="favorites-content">
+      <h5>Favorites</h5>
+      <div className="favorites-container">
+        {favorites.map((item) => {
+          const { idMeal, strMealThumb: image } = item;
+
+          return <div key={idMeal} className="favorite-item" >
+            <img src={image} className="favorites-img img" />
+            <button className='remove-btn' onClick={() => removeFromFavorites(idMeal)}><MdCancel /></button>
+          </div>
+        })}
+      </div>
+    </div>
+  </section>
 }
-export default Favorites;
+
+
+export default Favorites
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,27 +1,36 @@
 import { useState } from 'react'
-import { useGlobalContext } '..context'
+import { useGlobalContext } from '../context'
+
+
 
 const Search = () => {
+  const { setSearchTerm, fetchRandomMeal } = useGlobalContext()
+  const [text, setText] = useState('')
 
-  const { setSearchTerm } = useGlobalContext ()
-const [text, setText] = useState('')
   const handleChange = (e) => {
     setText(e.target.value)
   }
-const handleSubmit = (e) => {
-  e.preventDefault()
-  if (text) {
-    setSearchTerm(text)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text) {
+      setSearchTerm(text)
+    }
   }
-}
-  
+
+  const handleRandomMeal = () => {
+    setSearchTerm('')
+    setText('')
+    fetchRandomMeal()
+  }
+
   return <header className='search-container'>
-  <form onSubmit={handleSubmit} >
-    <input type='text' onChange={handleChange} value={text} placeholder='Type your favorite meal' className='form-input' />
-    <button type='submit' classNmae='btn'>Search</button>
-    <button type='button' className='btn btn-hipster'>Suprise Me!</button>
-  </form>
+    <form onSubmit={handleSubmit} >
+      <input type='text' onChange={handleChange} value={text} placeholder='type favorite meal' className='form-input' />
+      <button type="submit" className="btn">search</button>
+      <button type="btn" className="btn btn-hipster" onClick={handleRandomMeal}>Suprise Me !</button>
+    </form>
   </header>
 }
 
-export default Search;
+
+export default Search
